ba88080d69f31a4359a664eda489660bd82a468b
AFSantana <andres93-12@hotmail.com>
2022-02-02 20:55:01 -0500

    adición de descuento por cupones e inserción de imágen

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,10 @@
-//const precioOriginal = 120;
-//const descuento = 18;
+const cupones = [
+    "S117MC",
+    "B312N6",
+    "A239N4",
+    "A259N1",
+    "M343GS",
+];
 
 function calcularPrecioConDescuento(precio, descuento)
 {
@@ -8,22 +13,56 @@ function calcularPrecioConDescuento(precio, descuento)
     return precioConDescuento;
 }
 
+function calcularPrecioConDescuentoCupon(precio)
+{
+    const cupon = document.getElementById("InputVoucher");
+    const cuponValue = cupon.value;
+    let descuentoCupon;
+
+    switch(cuponValue)
+    {
+        case cupones[0]:
+            descuentoCupon = 25;
+        break;
+        case cupones[1]:
+            descuentoCupon = 20;
+        break;
+        case cupones[2]:
+            descuentoCupon = 15;
+        break;
+        case cupones[3]:
+            descuentoCupon = 10;
+        break;
+        case cupones[4]:
+            descuentoCupon = 5;
+        break;
+    }
+
+    const porcentajePrecioConCupon = 100 - descuentoCupon;
+    const precioConDescuentoYCupon = (precio * porcentajePrecioConCupon) / 100;
+    return precioConDescuentoYCupon;
+}
+
 function onClickButtonPriceDiscount()
 {
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
     const inputDiscount = document.getElementById("InputDiscount");
     const discountValue = inputDiscount.value;
-
+    
     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
-
+    const total = calcularPrecioConDescuentoCupon(precioConDescuento);
+    
     const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
-}
 
-//console.log({
-//   precioOriginal,
-// descuento,
-//porcentajePrecioConDescuento,
-//precioConDescuento
-//});
+    if(total>=0)
+    {
+        resultP.innerText = "El precio a pagar es: $" + total + " (Con cupón).";;
+    }
+    else
+    {
+        resultP.innerText = "El precio a pagar es: $" + precioConDescuento + " (Sin cupón).";
+    }
+    
+    
+}
